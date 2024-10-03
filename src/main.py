09e8e151cb2907c35dbfac8f3a8ab42ee41a63e2
Eduard Gimaev <e.gimaev@mail.ru>
2024-10-04 04:32:18 +1000

    src

---
+++ src/main.py
@@ -0,0 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+print(get_mask_card_number(2200358209833331))
+print(get_mask_account(56873457634785634876))
---
+++ src/masks.py
@@ -0,0 +1,12 @@
+def get_mask_card_number(card_number: int) -> str:
+    """Функция, которая маскирует номер карты"""
+
+    card_number_str = str(card_number)
+    return f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[12:]}"
+
+
+def get_mask_account(account_number: int) -> str:
+    """Функция, которая маскирует номер счета"""
+
+    account_number_str = str(account_number)
+    return f"**{account_number_str[-4:]}"
