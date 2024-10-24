f3baf4e0c4bdf549eec6f3aa4317e386d8f66042
Eduard Gimaev <e.gimaev@mail.ru>
2024-10-25 01:02:23 +1000

    fixes READMI.md

---
+++ src/processing.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+
+def filter_by_state(list_dictionary: list[dict[str, Any]], state_id: str = "EXECUTED") -> list[dict[str, Any]]:
+    """ "Функция, фильтрующая словари по указанному параметру"""
+
+    new_list = []
+
+    for key in list_dictionary:
+        if key.get("state") == state_id:
+            new_list.append(key)
+    return new_list
+
+
+def sort_by_date(list_dictionary: list[dict[str, Any]], revers: bool = True) -> list[dict[str, Any]]:
+    """Функция сортирующая список по дате"""
+
+    sorted_list = sorted(list_dictionary, key=lambda x: x["date"], reverse=revers)
+    return sorted_list
