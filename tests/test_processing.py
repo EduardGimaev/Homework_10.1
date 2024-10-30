4d184d8120205d987116fbf9e2a894e08f077cac
Eduard Gimaev <e.gimaev@mail.ru>
2024-10-30 23:30:46 +1000

    fixes processing

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -27,7 +27,7 @@ def test_filter_by_state_2(list_dict):
 
 
 @pytest.mark.parametrize("list_dictionary, expected", [
-    ( [
+    ([
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
@@ -38,7 +38,19 @@ def test_filter_by_state_2(list_dict):
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
         {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
         {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-    ]
-)])
+    ]),
+([
+        {"id": 41428829, "state": "EXECUTED", "date": "2021-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2020-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-10-14T08:21:33.419441"},
+    ],
+    [
+        {'id': 41428829, 'state': 'EXECUTED', 'date': '2021-07-03T18:35:29.512364'},
+        {'id': 594226727, 'state': 'CANCELED', 'date': '2020-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED', 'date': '2019-10-14T08:21:33.419441'},
+        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+    ])
+])
 def test_sort_by_date(list_dictionary, expected):
     assert sort_by_date(list_dictionary) == expected
