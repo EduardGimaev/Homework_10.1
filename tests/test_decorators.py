d04a92f4e666caecd950f4aabb2106eaa3c576e3
Eduard Gimaev <e.gimaev@mail.ru>
2024-11-22 23:47:50 +1000

    new test decorators

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,1 +1,18 @@
-from mypy.dmypy.client import statu
+from src.decorators import log
+
+
+@log()
+def my_function(x, y):
+    return x + y
+
+
+def test_log_console_ok(capsys):
+    my_function(2, 3)
+    output = capsys.readouterr()
+    assert output.out == "my_function ok\n"
+
+
+def test_log_console_error(capsys):
+    my_function(2, "3")
+    output = capsys.readouterr()
+    assert output.out == "my_function error: TypeError. Inputs: (2, '3'), {}\n"
